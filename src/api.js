a72882e23fe9eaaf9185c4211192e6f0b3428469
Wiktoria Skibińska <wikas2000@icloud.com>
2023-12-18 15:11:49 +0100

    gallery rendering

--- src/api.js
+++
@@ -1,5 +0,0 @@
-const url = `https://pixabay.com/api/`;
-const KEY = `41240894-272bca1f2c3dcb1548b81eb12`;
-const fetchImages = async () => {
-  const data = await fetch(url);
-};
--- src/index.js
+++ src/index.js
@@ -1,35 +1,100 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
 const input = document.querySelector('input');
-const button = document.querySelector('button');
+const searchBttn = document.querySelector('button');
 const form = document.querySelector('form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBttn = document.querySelector('.load-more');
+let queryValue = '';
+let currentPage = '';
+loadMoreBttn.style.display = 'none';
+const API_KEY = `41240894-272bca1f2c3dcb1548b81eb12`;
 
 form.addEventListener('submit', onFormSubmit);
+
 function onFormSubmit(evt) {
   evt.preventDefault();
-  const queryValue = evt.currentTarget.value;
+  queryValue = evt.currentTarget.elements.searchQuery.value;
+  currentPage = 1;
+  gallery.innerHTML = '';
   fetchImages();
 }
 
-const API_KEY = `41240894-272bca1f2c3dcb1548b81eb12`;
-const URL =
-  'https://pixabay.com/api/?key=' +
-  API_KEY +
-  '&q=flower' +
-  '&image_type=photo' +
-  '&orientation=horizontal' +
-  '&safesearxh=true' +
-  '&per_page=40';
+loadMoreBttn.addEventListener('click', () => {
+  fetchImages();
+});
 
-const fetchImages = async () => {
+async function fetchImages() {
   try {
-    const response = await axios.get(URL);
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(
+        queryValue
+      )}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=40`
+    );
     const images = await response.data;
     console.log(images);
+    if (images.hits.length === 0) {
+      loadMoreBttn.style.display = 'none';
+      Notiflix.Report.failure(
+        'Sorry!',
+        'No images found!Please try again :)',
+        'OK'
+      );
+    }
+    currentPage += 1;
+    loadMoreBttn.style.display = 'block';
+
+    if (currentPage === 2) {
+      Notiflix.Notify.success(`Hooray! We found ${images.totalHits} images.`, {
+        position: 'center-top',
+      });
+    }
+
+    renderGallery(images.hits);
+    if (currentPage > Math.ceil(images.totalHits / 40)) {
+      loadMoreBttn.style.display = 'none';
+      Notiflix.Report.info(
+        'End of results',
+        'sorry you have reached end of the results',
+        'OK'
+      );
+    }
   } catch (error) {
+    loadMoreBttn.style.display = 'none';
+    Notiflix.Report.failure(
+      'Error!',
+      'There was an error! Please try again',
+      'OK'
+    );
     console.log(error);
   }
-};
+}
+
+function renderGallery(images) {
+  const markup = images
+    .map(image => {
+      return `<div class="photo-card">
+      <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${image.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${image.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${image.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads:${image.downloads}</b>
+    </p>
+  </div>
+</div>`;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
 /*key - Twój unikalny klucz dostępu do API.
 q - termin, który chcemy wyszukać. W tej aplikacji to treść którą będzie wpisywał użytkownik.
 image_type - typ obrazka. Chcemy tylko zdjęcia, dlatego określ wartość parametru jako photo.
